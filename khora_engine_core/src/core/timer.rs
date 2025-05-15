c3b73090759aad552ffb6c0451beadbe80af4c4a
fm <81640753+eraflo@users.noreply.github.com>
2025-05-15 21:28:40 +0300

    change test timer margin for mac

--- khora_engine_core/src/core/timer.rs
+++ khora_engine_core/src/core/timer.rs
@@ -70,7 +70,7 @@ mod tests {
 
     const SMALL_DURATION_MS: u64 = 15;
     const SLEEP_DURATION_MS: u64 = 100;
-    const SLEEP_MARGIN_MS: u64 = 50;
+    const SLEEP_MARGIN_MS: u64 = 100;
 
     /// A test to check if the Stopwatch struct is created correctly and starts the timer.
     /// It verifies that the elapsed time is not None after creation and that it is very small.
