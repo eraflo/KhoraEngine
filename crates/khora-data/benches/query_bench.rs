3bc0aaaa56efb05a835a079f4b618f977a10216d
Florian Maillard <81640753+eraflo@users.noreply.github.com>
2026-02-06 22:45:03 +0100

    fix: delete criterion black_box, use hint black box

--- crates/khora-data/benches/query_bench.rs
+++ crates/khora-data/benches/query_bench.rs
@@ -1,5 +1,6 @@
-use criterion::{black_box, criterion_group, criterion_main, Criterion};
+use criterion::{criterion_group, criterion_main, Criterion};
 use khora_data::ecs::{Component, SemanticDomain, World};
+use std::hint::black_box;
 
 #[derive(Debug, Clone, Copy, Default)]
 struct Position(u32);
